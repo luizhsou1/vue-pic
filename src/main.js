8ca05dc5213a0fbb0fd821b3d3ebe8c5f6906fb9
Luiz Henrique <luihsou1@gmail.com>
2020-08-02 14:34:50 -0300

    finalizacao do projeto

--- src/main.js
+++ src/main.js
@@ -4,14 +4,31 @@ import VueResource from 'vue-resource';
 import VueRouter from 'vue-router';
 import { routes } from './routes';
 import './directives/Transform';
-
+import VeeValidate from 'vee-validate';
+import msg from './pt_BR';
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/teste.css';
+import './assets/js/teste.js';
+import 'bootstrap/dist/js/bootstrap.js';
 
 Vue.use(VueResource);
+Vue.http.options.root = 'http://localhost:3000';
+
 Vue.use(VueRouter);
 
 const router = new VueRouter({ 
   routes, 
   mode: 'history'
+});
+
+Vue.use(VeeValidate, {
+  
+  locale: 'pt_BR',
+  dictionary: {
+    pt_BR: {
+      messages: msg
+    }
+  }
 });
 
 new Vue({
